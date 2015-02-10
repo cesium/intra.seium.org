85a6469028bd2bca64d68baa0268039074405a0c
Pedro Carneiro <pedrosfdcarneiro@gmail.com>
2015-02-10 23:44:11 +0000

    Update views with avatar info

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-	has_attached_file :avatar, styles: {  medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :avatar, styles: {  medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/avatar-missing.png"
 	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
 	has_and_belongs_to_many :activities
