3d296f61fd28474a8577902b1e9162d95a6915ce
JoaoFCosta <dinojoaocosta@gmail.com>
2015-02-12 17:37:01 +0000

    Finish user show page Add function to check if user has any social links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,19 @@ class UsersController < ApplicationController
 	end
 
 	def show
+		@social = check_accounts
 		respond_with(@user)
 	end
 
 	private
+
+	# Check if there are any social accounts
+	def check_accounts
+		( @user.facebook_account != nil && @user.facebook_account != "" ) ||
+		( @user.twitter_account != nil && @user.twitter_account != "" ) ||
+		( @user.github_account != nil && @user.github_account != "" ) ||
+		( @user.google_plus_account != nil && @user.google_plus_account != "" )
+	end
 
 	def set_user
 		@user = User.where(username: params[:username]).first.public_profile
