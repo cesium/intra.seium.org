19beff7382cafe6e4e9c7403a65b85f8ea7e62ae
JoRod94 <joao.rod94@gmail.com>
2015-02-13 01:25:57 +0000

    Added badge views

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+
 ActiveRecord::Schema.define(version: 20150212192910) do
 
   create_table "activities", force: true do |t|
