6e5264ce01a04622ef86fe3362b41494ac09f9a0
Pedro Carneiro <pedrosfdcarneiro@gmail.com>
2015-02-16 04:34:08 +0000

    Users seed removed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,7 @@ require './db/seeds_files/editions'
 require './db/seeds_files/companies'
 require './db/seeds_files/speakers'
 require './db/seeds_files/activities'
-require './db/seeds_files/staff'
-require './db/seeds_files/users'
 require './db/seeds_files/badges'
-require './db/seeds_files/badge_acquisitions'
 
 # Editions
 editions = Seed::editions
@@ -21,12 +18,3 @@ activities = Seed::activities(editions, speakers)
 
 # Badges
 badges = Seed::badges(editions, activities)
-
-# Staff
-staff = Seed::staff(editions, activities)
-
-# Users
-users = Seed::users(editions, activities)
-
-# Badges Acquisition
-#badge_acquisitions = Seed::badge_acquisitions(users, badges)
