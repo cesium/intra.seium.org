29b5b6447f43347054cf03b1f79a150118cbd494
Bruno Ferreira <chalkos@chalkos.net>
2015-02-16 23:41:33 +0000

    Adicionados badges para terça feira

--- db/seeds_files/new_badges.rb
+++ db/seeds_files/new_badges.rb
@@ -7,7 +7,49 @@ module RakeBadgeCreation
 				badge_type: BadgeType::CODE_NEEDED,
 				logo_url: '/images/badges/participacao/16/conheci_helder_pinto.jpg',
 				edition_id: 2015
-			}
+			},
+			{
+				name: "Amigo da Keep Solutions",
+				codename: "keepsolutions_buddy",
+				badge_type: BadgeType::CODE_NEEDED,
+				logo_url: '/images/badges/keepsolutions_buddy.png',
+				edition: 2015
+			},
+			{
+				name: "Amigo da Microsoft",
+				codename: "microsoft_buddy",
+				badge_type: BadgeType::CODE_NEEDED,
+				logo_url: '/images/badges/microsoft_buddy.png',
+				edition: 2015
+			},
+			{
+				name: "Participação na Palestra de Unity 3D",
+				codename: "palestra_unity3d",
+				badge_type: BadgeType::CODE_NEEDED,
+				logo_url: '/images/badges/palestra_unity3d.png',
+				edition: 2015
+			},
+			{
+				name: "Participação na Palestra de Product Development & Agile",
+				codename: "palestra_pd_agile",
+				badge_type: BadgeType::CODE_NEEDED,
+				logo_url: '/images/badges/palestra_pd_agile.png',
+				edition: 2015
+			},
+			{
+				name: "Participação na Palestra de Xamarin",
+				codename: "palestra_xamarin",
+				badge_type: BadgeType::CODE_NEEDED,
+				logo_url: '/images/badges/palestra_xamarin.png',
+				edition: 2015
+			},
+			{
+				name: "Participação na Palestra de Meteor",
+				codename: "palestra_meteor",
+				badge_type: BadgeType::CODE_NEEDED,
+				logo_url: '/images/badges/palestra_meteor.png',
+				edition: 2015
+			},
 		]
 	end
 end
