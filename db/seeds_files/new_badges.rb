d76c4a49fa1aa36911b358604abd6a528d005ed8
Francisco Neves <fntneves@gmail.com>
2015-02-17 10:54:38 +0000

    Add badges

--- db/seeds_files/new_badges.rb
+++ db/seeds_files/new_badges.rb
@@ -42,6 +42,97 @@ module RakeBadgeCreation
 				badge_type: BadgeType::CODE_NEEDED,
 				logo_url: '/images/badges/palestra_meteor.png',
 				edition_id: 2015
+			},
+			{
+				name: "Participação na Palestra de Meteor",
+				codename: "palestra_meteor",
+				badge_type: BadgeType::CODE_NEEDED,
+				logo_url: '/images/badges/palestra_meteor.png',
+				edition_id: 2015
+			},
+			{
+				name: "Encontra o badge que está num membro da organização",
+				codename: "hide_n_seek",
+				badge_type: BadgeType::CODE_NEEDED,
+				logo_url: '/images/badges/sei/hide_n_seek.png',
+				edition_id: 2015
+			},
+			{
+				name: "Encontra um bug na plataforma da SEI",
+				codename: "bugtracker",
+				badge_type: BadgeType::CODE_NEEDED,
+				logo_url: '/images/badges/sei/bugtracker.png',
+				edition_id: 2015
+			},
+			{
+				name: "Speaker na SEI",
+				codename: "speaker",
+				badge_type: BadgeType::CODE_NEEDED,
+				logo_url: '/images/badges/sei/speaker.png',
+				edition_id: 2015
+			},
+			{
+				name: "Faz um Pull-Request para a plataforma da SEI",
+				codename: "pullrequest",
+				badge_type: BadgeType::CODE_NEEDED,
+				logo_url: '/images/badges/sei/pullrequest.png',
+				edition_id: 2015
+			},
+			{
+				name: "Conhece o Fernando Mendes",
+				codename: "precocerto",
+				badge_type: BadgeType::CODE_NEEDED,
+				logo_url: '/images/badges/sei/precocerto.png',
+				edition_id: 2015
+			},
+			{
+				name: "Procura-os no Departamento de Informática",
+				codename: "treasure",
+				badge_type: BadgeType::CODE_NEEDED,
+				logo_url: '/images/badges/sei/treasure.png',
+				edition_id: 2015
+			},
+			{
+				name: "Faço tudo por um badge!"
+				codename: "idonteven",
+				badge_type: BadgeType::CODE_NEEDED,
+				logo_url: '/images/badges/sei/idonteven.png',
+				edition_id: 2015
+			},
+			{
+				name: "Rei dos Coffee Breaks"
+				codename: "uhomnon",
+				badge_type: BadgeType::CODE_NEEDED,
+				logo_url: '/images/badges/sei/uhomnon.png',
+				edition_id: 2015
+			},
+			{
+				name: "Deixou-nos orgulhosos"
+				codename: "awesome",
+				badge_type: BadgeType::CODE_NEEDED,
+				logo_url: '/images/badges/sei/awesome.png',
+				edition_id: 2015
+			},
+			{
+				name: "Entregue a quem fizer uma declaração pública de amor à SEI"
+				codename: "lover",
+				badge_type: BadgeType::CODE_NEEDED,
+				logo_url: '/images/badges/sei/lover.png',
+				edition_id: 2015
+			},
+			{
+				name: "Amigo da Microsoft"
+				codename: "microsoft",
+				badge_type: BadgeType::CODE_NEEDED,
+				logo_url: '/images/badges/sei/microsoft.png',
+				edition_id: 2015
+			},
+			{
+				name: "Ninguém sabe ao certo como se ganha este badge. Há quem diga que não existe."
+				codename: "wat",
+				badge_type: BadgeType::CODE_NEEDED,
+				logo_url: '/images/badges/sei/wat.png',
+				edition_id: 2015
 			}
 		]
 	end
