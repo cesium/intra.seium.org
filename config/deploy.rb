47cf85aaddba0ff76d2264d26fc66399b5d7113b
Bruno Ferreira <chalkos@chalkos.net>
2016-02-06 00:19:54 +0000

    added seed stage

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,6 +15,17 @@ namespace :deploy do
     end
   end
 
+  desc 'Runs rake db:seed'
+  task :seed => [:set_rails_env] do
+    on primary fetch(:migration_role) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, "db:seed"
+        end
+      end
+    end
+  end
+
   after :publishing, 'deploy:restart'
   after :finishing, 'deploy:cleanup'
 end
