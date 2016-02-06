2928d4a818d1f06720d24434e290e8f5aba01d75
Bruno Ferreira <chalkos@chalkos.net>
2016-02-06 00:01:23 +0000

    changed root page

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-	root to: 'editions#show', id: 2015
+	root to: 'intranet#intranet'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
