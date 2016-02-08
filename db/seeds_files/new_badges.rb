8f1a20f9cf2754ea52fe4372c545e3b4aab92ad0
JoaoFCosta <dinojoaocosta@gmail.com>
2016-02-08 09:33:25 +0000

    Update new_badges.rb

--- db/seeds_files/new_badges.rb
+++ db/seeds_files/new_badges.rb
@@ -1,461 +1,104 @@
 module RakeBadgeCreation
-	def self.badges
-		[
-			{
-				name: "Conheci o Hélder Pinto",
-				codename: "helder_pinto",
+	def self.badges(editions, activities)
+		Badge.create!([
+      {
+				name: "Amigo da Blip",
+				codename: "blip_buddy",
 				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/participacao/16/conheci_helder_pinto.jpg',
-				edition_id: 2015
+				logo_url: '/images/badges/16/enterprises/blip.png',
+				edition_id: 2016
 			},
 			{
-				name: "Amigo da Keep Solutions",
-				codename: "keepsolutions_buddy",
+				name: "Amigo da BloomIdea",
+				codename: "bloomidea_buddy",
 				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/keepsolutions_buddy.png',
-				edition_id: 2015
+				logo_url: '/images/badges/16/enterprises/bloomidea.png',
+				edition_id: 2016
 			},
-			{
-				name: "Participação na Palestra de Unity 3D",
-				codename: "palestra_unity3d",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/palestra_unity3d.png',
-				edition_id: 2015
-			},
-			{
-				name: "Participação na Palestra de Product Development & Agile",
-				codename: "palestra_pd_agile",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/palestra_pd_agile.png',
-				edition_id: 2015
-			},
-			{
-				name: "Participação na Palestra de Xamarin",
-				codename: "palestra_xamarin",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/palestra_xamarin.png',
-				edition_id: 2015
-			},
-			{
-				name: "Participação na Palestra de Meteor",
-				codename: "palestra_meteor",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/palestra_meteor.png',
-				edition_id: 2015
-			},
-			{
-				name: "Encontra o badge que está num membro da organização",
-				codename: "hide_n_seek",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/sei/hide_n_seek.png',
-				edition_id: 2015
-			},
-			{
-				name: "Encontra um bug na plataforma da SEI",
-				codename: "bugtracker",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/sei/bugtracker.png',
-				edition_id: 2015
-			},
-			{
-				name: "Speaker na SEI",
-				codename: "speaker",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/sei/speaker.png',
-				edition_id: 2015
-			},
-			{
-				name: "Faz um Pull-Request para a plataforma da SEI",
-				codename: "pullrequest",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/sei/pullrequest.png',
-				edition_id: 2015
-			},
-			{
-				name: "Conhece o Fernando Mendes",
-				codename: "precocerto",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/sei/precocerto.png',
-				edition_id: 2015
-			},
-			{
-				name: "Procura-os no Departamento de Informática",
-				codename: "treasure",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/sei/treasure.png',
-				edition_id: 2015
-			},
-			{
-				name: "Faço tudo por um badge!",
-				codename: "idonteven",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/sei/idonteven.png',
-				edition_id: 2015
-			},
-			{
-				name: "Rei dos Coffee Breaks",
-				codename: "uhomnon",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/sei/uhomnon.png',
-				edition_id: 2015
-			},
-			{
-				name: "Deixou-nos orgulhosos",
-				codename: "awesome",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/sei/awesome.png',
-				edition_id: 2015
-			},
-			{
-				name: "Entregue a quem fizer uma declaração pública de amor à SEI",
-				codename: "lover",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/sei/lover.png',
-				edition_id: 2015
-			},
-			{
-				name: "Amigo da Microsoft",
-				codename: "microsoft",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/sei/microsoft.png',
-				edition_id: 2015
-			},
-			{
-				name: "Ninguém sabe ao certo como se ganha este badge. Há quem diga que não existe.",
-				codename: "wat",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/sei/wat.png',
-				edition_id: 2015
-			},
-			{
-				name: "Participação na Palestra de API Driven Development",
-				codename: "palestra_api",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/palestra_api.png',
-				edition_id: 2015
-			},
-			{
-				name: "Participação na Reunião Geral de Alunos",
-				codename: "rga_participation",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/rga_participation.png',
-				edition_id: 2015
-			},
-			{
-				name: "Colocou questões na Reunião Geral de Alunos",
-				codename: "rga_question",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/rga_question.png',
-				edition_id: 2015
-			},
-			{
-				name: "Participação na Corrida Social",
-				codename: "run_participation",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/run_participation.png',
-				edition_id: 2015
-			},
-			{
-				name: "Vencedor da Corrida Social",
-				codename: "run_winner",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/run_winner.png',
-				edition_id: 2015
-			},
-			{
-				name: "Participação no dia 17",
-				codename: "attendee_17",
-				badge_type: BadgeType::USER_ACCOUNT,
-				logo_url: '/images/badges/attendee_17.png',
-				edition_id: 2015
-			},
-			{
-				name: "Participação no dia 18",
-				codename: "attendee_18",
-				badge_type: BadgeType::USER_ACCOUNT,
-				logo_url: '/images/badges/attendee_18.png',
-				edition_id: 2015
-			},
-			{
-				name: "Participação no dia 19",
-				codename: "attendee_19",
-				badge_type: BadgeType::USER_ACCOUNT,
-				logo_url: '/images/badges/attendee_19.png',
-				edition_id: 2015
-			},
-			{
-				name: "Participação na Palestra de Behaviour Driven Development",
-				codename: "palestra_bdd",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/palestra_bdd.png',
-				edition_id: 2015
-			},
-			{
-				name: "Participação no Workshop de Modelação 3D",
-				codename: "workshop_modelacao3d",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/workshop_modelacao3d.png',
-				edition_id: 2015
-			},
-			{
-				name: "Participação no Workshop de Impressão 3D",
-				codename: "workshop_impressao3d",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/workshop_impressao3d.png',
-				edition_id: 2015
-			},
-			{
-				name: "Participação no Workshop de Swift",
-				codename: "workshop_swift",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/workshop_swift.png',
-				edition_id: 2015
-			},
-			{
-				name: "Participação na Palestra de DevOps",
-				codename: "palestra_devops",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/palestra_devops.png',
-				edition_id: 2015
-			},
-			{
-				name: "Participação no Quiz Show",
-				codename: "quiz_attendee",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/quiz_attendee.png',
-				edition_id: 2015
-			},
-			{
-				name: "Vencedor do Quiz Show",
-				codename: "quiz_winner",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/quiz_winner.png',
-				edition_id: 2015
-			},
-			{
-				name: "Participação no Workshop de HTML5 e CSS3",
-				codename: "workshop_htmlcss",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/workshop_htmlcss.png',
-				edition_id: 2015
-			},
-			{
-				name: "Participação no Workshop de Arduino",
-				codename: "workshop_arduino",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/workshop_arduino.png',
-				edition_id: 2015
-			},
-			{
-				name: "Participação no Workshop de Python + web2py",
-				codename: "workshop_python_web2py",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/workshop_python_web2py.png',
-				edition_id: 2015
-			},
-			{
-				name: "Participação no Workshop de Drones",
-				codename: "workshop_drones",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: nil, 
-				edition_id: 2015
-			},
-			{
-				name: "Participação na Palestra de User Experience",
-				codename: "palestra_user_experience",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/palestra_user_experience.png',
-				edition_id: 2015
-			},
-			{
-				name: "Participação na Palestra de Rust",
-				codename: "palestra_rust",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/palestra_rust.png',
-				edition_id: 2015
-			},
-			{
-				name: "Participação na Palestra de Startup Experience",
-				codename: "palestra_startup_experience",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: nil, 
-				edition_id: 2015
-			},
-			{
-				name: "Vencedor do desafio de colecionar badges",
-				codename: "badges_winner",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: nil, 
-				edition_id: 2015
-			},
-			{
-				name: "Melhor publicação Twitter #seiuminho",
-				codename: "best_twitter",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/best_twitter.png',
-				edition_id: 2015
-			},
-			{
-				name: "Melhor publicação Facebook #seiuminho",
-				codename: "best_facebook",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/best_facebook.png',
-				edition_id: 2015
-			},
-			{
-				name: "Melhor foto publicada nas redes sociais #seiuminho",
-				codename: "best_photo",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/best_photo.png',
-				edition_id: 2015
-			},
-			{
-				name: "Vencedor do desafio Build Your MVP",
-				codename: "challenge_build_mvp_winner",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: nil, 
-				edition_id: 2015
-			},
-			{
-				name: "Participante no desafio Build Your MVP",
-				codename: "challenge_build_mvp_attendee",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: nil, 
-				edition_id: 2015
-			},
-			{
-				name: "Vencedor da Bitreserve Challenge",
-				codename: "challenge_bitreserve_winner",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: nil, 
-				edition_id: 2015
-			},
-			{
-				name: "Participante na Bitreserve Challenge",
-				codename: "challenge_bitreserve_attendee",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: nil, 
-				edition_id: 2015
-			},
-			{
-				name: "Vencedor da VILT Challenge",
-				codename: "challenge_vilt_winner",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: nil, 
-				edition_id: 2015
-			},
-			{
-				name: "Participante na VILT Challenge",
-				codename: "challenge_vilt_attendee",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/challenge_vilt_attendee.png',
-				edition_id: 2015
-			},
-			{
-				name: "Completou o 1º desafio de segurança",
-				codename: "challenge_security_1",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/challenge_security_1.png',
-				edition_id: 2015
-			},
-			{
-				name: "Completou o 2º desafio de segurança",
-				codename: "challenge_security_2",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/challenge_security_2.png',
-				edition_id: 2015
-			},
-			{
-				name: "Completou o 3º desafio de segurança",
-				codename: "challenge_security_3",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/challenge_security_3.png',
-				edition_id: 2015
-			},
-			{
-				name: "Vencedor do desafio de segurança",
-				codename: "challenge_security_winner",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/challenge_security_winner.png',
-				edition_id: 2015
-			},
-			{
-				name: "Vencedor das Time Trial Talks",
-				codename: "challenge_ttt_winner",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: nil, 
-				edition_id: 2015
-			},
-			{
-				name: "Participante nas Time Trial Talks",
-				codename: "challenge_ttt_attendee",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: nil, 
-				edition_id: 2015
-			},
-			{
-				name: "Vencedor do Presentation Karaoke",
-				codename: "challenge_karaoke_winner",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: nil, 
-				edition_id: 2015
-			},
-			{
-				name: "participante no presentation karaoke",
-				codename: "challenge_karaoke_attendee",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: nil, 
-				edition_id: 2015
-			},
-			{
-				name: "Participação no Jantar de Encerramento",
-				codename: "attendee_dinner",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: '/images/badges/attendee_dinner.png',
-				edition_id: 2015
-			},
-			{
-				name: "Participação na hackathon",
-				codename: "attendee_hackathon",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: nil, 
-				edition_id: 2015
-			},
-			{
-				name: "Participação no dia 20",
-				codename: "attendee_20",
-				badge_type: BadgeType::USER_ACCOUNT,
-				logo_url: '/images/badges/attendee_20.png',
-				edition_id: 2015
-			},
-			{
-				name: "Participação no dia 21",
-				codename: "attendee_21",
-				badge_type: BadgeType::USER_ACCOUNT,
-				logo_url: '/images/badges/attendee_21.png',
-				edition_id: 2015
-			},
-			{
-				name: "Amigo da Startup Braga",
-				codename: "startupbraga_buddy",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: nil,
-				edition_id: 2015
-			},
-			{
-				name: "Amigo da VILT",
-				codename: "vilt_buddy",
-				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: nil, 
-				edition_id: 2015
-			},
-			{
+      {
 				name: "Amigo da Eurotux",
 				codename: "eurotux_buddy",
 				badge_type: BadgeType::CODE_NEEDED,
-				logo_url: nil, 
-				edition_id: 2015
+				logo_url: '/images/badges/16/enterprises/eurotux.png',
+				edition_id: 2016
+			},
+      {
+				name: "Amigo da Farfetch",
+				codename: "farfetch_buddy",
+				badge_type: BadgeType::CODE_NEEDED,
+				logo_url: '/images/badges/16/enterprises/farfetch.png',
+				edition_id: 2016
+			},
+      {
+				name: "Amigo da OutSystems",
+				codename: "outsystems_buddy",
+				badge_type: BadgeType::CODE_NEEDED,
+				logo_url: '/images/badges/16/enterprises/outsystems.png',
+				edition_id: 2016
+			},
+      {
+				name: "Amigo da Seegno",
+				codename: "seegno_buddy",
+				badge_type: BadgeType::CODE_NEEDED,
+				logo_url: '/images/badges/16/enterprises/seegno.png',
+				edition_id: 2016
+			},
+      {
+				name: "Amigo da Subvisual",
+				codename: "subvisual_buddy",
+				badge_type: BadgeType::CODE_NEEDED,
+				logo_url: '/images/badges/16/enterprises/subvisual.png',
+				edition_id: 2016
+			},
+      {
+				name: "Levantar Kit",
+				codename: "unwrap_kit",
+				badge_type: BadgeType::CODE_NEEDED,
+				logo_url: '/images/badges/16/random/kit.png',
+				edition_id: 2016
+			},
+      {
+				name: "Acordar cedo",
+				codename: "early_bird",
+				badge_type: BadgeType::CODE_NEEDED,
+				logo_url: '/images/badges/16/random/early_bird.png',
+				edition_id: 2016
+			},
+      {
+				name: "Participação na palestra sobre BITalino",
+				codename: "bitalino",
+				badge_type: BadgeType::CODE_NEEDED,
+				logo_url: '/images/badges/16/activities/monday/bitalino.png',
+				edition_id: 2016
+			},
+      {
+				name: "Eu arranjei um Mac, iSwift",
+				codename: "swift",
+				badge_type: BadgeType::CODE_NEEDED,
+				logo_url: '/images/badges/16/activities/monday/swift.png',
+				edition_id: 2016
+			},
+      {
+				name: "Wannabe Empreendedor",
+				codename: "entrepreneur_celso",
+				badge_type: BadgeType::CODE_NEEDED,
+				logo_url: '/images/badges/16/activities/monday/entrepreneur.png',
+				edition_id: 2016
+			},
+      {
+				name: "Começar com o pé direito",
+				codename: "opening_ceremony",
+				badge_type: BadgeType::CODE_NEEDED,
+				logo_url: '/images/badges/16/activities/monday/opening_ceremony.png',
+				edition_id: 2016
+			},
+      {
+				name: "Participação em workshop de React e Redux",
+				codename: "react_redux",
+				badge_type: BadgeType::CODE_NEEDED,
+				logo_url: '/images/badges/16/activities/monday/react_redux.png',
+				edition_id: 2016
 			}
-		]
+		])
 	end
 end
