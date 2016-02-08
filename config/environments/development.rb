e09a36b390ddf8c1fa079c53a1aa3a58410fe973
0urobor0s <lhome.gamer@gmail.com>
2016-02-08 19:54:35 +0000

    Added more Badges

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  #
+  config.secret_key_base = ENV['SECRET_KEY_BASE']
 	
 	Paperclip.options[:command_path] = "/usr/bin/"
 end
