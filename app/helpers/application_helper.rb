94fc1b4973f283fc294ad8148c844772ba55539d
JoaoFCosta <dinojoaocosta@gmail.com>
2016-12-14 20:04:46 +0000

    Update navbar.
    - Update navbar from ERB to SLIM.
    - Delete duplicated navbar stylesheet file.
    - Change navbar background color and logo.
    - Change hover color of navbar links.
    - Create helper methods in `application_helper` to know which tab is active.
    - Update navbar active tab look.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,22 @@
 module ApplicationHelper
+  def is_participants_active?
+    is_active?(edition_participants_path(edition_id: @edition))
+  end
+
+  def is_badges_active?
+    is_active?(edition_badges_path(edition_id: @edition))
+  end
+
+  def is_redeem_badge_active?
+    is_active?(redeem_edition_badges_path(edition_id: @edition))
+  end
+
+  def is_profile_active?
+   is_active?(edition_participant_path(username: current_user.username, edition_id: @edition))
+  end
+
+  private
+
   def is_active?(link_path)
     current_page?(link_path) ? "active" : ""
   end
