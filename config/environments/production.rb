7407f7e070596a04df7df3f426bd7a8acb60da1c
JoaoFCosta <dinojoaocosta@gmail.com>
2017-01-24 21:11:42 +0000

    Update environments configuration files for S3.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,13 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 	Paperclip.options[:command_path] = "/usr/bin/"
+  config.paperclip_defaults = {
+    storage: s3,
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION')
+    }
+  }
 end
