d2db94e7b58a2d4946fcaa64509775272588b65e
JoaoFCosta <dinojoaocosta@gmail.com>
2017-01-24 21:22:55 +0000

    Update environments configuration files.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,12 +78,13 @@ Rails.application.configure do
 
 	Paperclip.options[:command_path] = "/usr/bin/"
   config.paperclip_defaults = {
-    storage: s3,
+    storage: :s3,
+    s3_region: ENV.fetch("AWS_REGION"),
     s3_credentials: {
-      bucket: ENV.fetch('S3_BUCKET_NAME'),
-      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('AWS_REGION')
+      bucket: ENV.fetch("S3_BUCKET_NAME"),
+      access_key_id: ENV.fetch("AWS_ACCESS_KEY_ID"),
+      secret_access_key: ENV.fetch("AWS_SECRET_ACCESS_KEY"),
+      s3_host_name: ENV.fetch("AWS_HOST_NAME"),
     }
   }
 end
