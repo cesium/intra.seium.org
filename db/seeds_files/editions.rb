b264262b4156bffe86402839cf11526effb10ee5
0urobor0s <lhome.gamer@gmail.com>
2018-01-31 00:00:36 +0000

    Add seed of 2018 edition
    Also remove a .swp file
    
    Remove 2017 edition

--- db/seeds_files/editions.rb
+++ db/seeds_files/editions.rb
@@ -1,14 +1,14 @@
 module Seed
-	def self.editions
-		Edition.create!([
-			{
-				id: 2017,
-				name: "III Semana de Engenharia Informática na Universidade do Minho",
-				edition_number: 3,
-				description: '3ª Edição da Semana da Engenharia Informática da Universidade do Minho',
-				begin_date: DateTime.civil_from_format(:local, 2017, 2, 4),
-				end_date: DateTime.civil_from_format(:local, 2017, 2, 11)
-			}
-		])
-	end
+  def self.editions
+    Edition.create!([
+      {
+        id: 2018,
+        name: "IV Semana de Engenharia Informática na Universidade do Minho",
+        edition_number: 4,
+        description: '4ª Edição da Semana da Engenharia Informática da Universidade do Minho',
+        begin_date: DateTime.civil_from_format(:local, 2018, 2, 3),
+        end_date: DateTime.civil_from_format(:local, 2018, 2, 10)
+      }
+    ])
+  end
 end
